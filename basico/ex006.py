792e6f3ca8d38eb577e8ac444fc2db5cd8f7ddce
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-24 14:00:08 -0300

    Create ex006.py

---
+++ basico/ex006.py
@@ -0,0 +1,7 @@
+num =   int(input("Digite um número:"))
+
+dobro = num * 2
+triplo = num * 3
+raiz = num ** (1/2)
+
+print(f"O Dobro do valor é: {dobro} \nO Triplo do valor é: {triplo} \nA raiz quadrada é: {raiz:.0f}")
