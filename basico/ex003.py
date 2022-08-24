3a28beb69b0fba8e7117cf3cd75aeee23da5564c
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-24 12:58:02 -0300

    Create ex003.py

---
+++ basico/ex003.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+#Podemos Somar os números usando uma variável ou simplesmente no print
+
+soma = num1 + num2
+
+print("A soma dos valores é {}".format(soma))
+
+# Ou
+
+print("A soma dos valores é {}".format(num1 + num2))
