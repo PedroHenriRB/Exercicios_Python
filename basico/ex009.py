5a2c07b2f3ba1e7f076464487a7c52abe5d16bf0
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-24 15:01:40 -0300

    Create ex009.py

---
+++ basico/ex009.py
@@ -0,0 +1,16 @@
+valor = int(input("Digite um número: "))
+
+print("-" * 10)
+
+print(f"{valor} x {1:2} = {valor * 1}")
+print(f"{valor} x {2:2} = {valor * 2}")
+print(f"{valor} x {3:2} = {valor * 3}")
+print(f"{valor} x {4:2} = {valor * 4}")
+print(f"{valor} x {5:2} = {valor * 5}")
+print(f"{valor} x {6:2} = {valor * 6}")
+print(f"{valor} x {7:2} = {valor * 7}")
+print(f"{valor} x {8:2} = {valor * 8}")
+print(f"{valor} x {9:2} = {valor * 9}")
+print(f"{valor} x {10:2} = {valor * 10}")
+
+print("-" * 10)
