e45b9785b3e2d36bb14feb4cfe0a54e366df6a9b
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-24 16:12:35 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-frase1 = input("Digite uma frase: ")
-frase2 = input("Digite uma frase: ")
+nome = input("Digite seu nome: ")
 
-print(f"String 1: {frase1}")
-print(f"String 2: {frase2}")
-
-print(f"Tamanho de \"{frase1}\":", len(frase1))
-print(f"Tamanho de \"{frase2}\":", len(frase2))
-
-if len(frase1) == len(frase2):
-    print("As duas Strings são de tamanhos iguais.")
-else:
-    print("As duas Strings são de tamanhos diferentes.")
-
-if frase1 == frase2:
-    print("As duas Strings possuem conteúdo iguais.")
-else:
-    print("As duas Strings possuem conteúdo diferentes.")
+print(nome.upper()[::-1])
