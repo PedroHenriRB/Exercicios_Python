0dd486ef55c3898b633bd6cd2ca40946d2fdf080
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-25 14:16:54 -0300

    Update ex016.py

--- basico/ex016.py
+++ basico/ex016.py
@@ -1,5 +1,13 @@
-from math import ceil, floor
+import math     #Poderíamos tbm só importar o trunc (from math import trunc).
 
 num = float(input("Digite um número: "))
 
-print(f"O número {num} tem a parte inteira {floor(num)}.")
+print(f"O número {num} tem a parte inteira {math.trunc(num)}.")     #trunc = trunca o número na sua forma Inteira
+
+"""
+Usando sem a biblioteca math
+
+num = float(input("Digite um número: "))
+
+print(f"O número {num} tem a parte inteira {int(num)})
+"""
