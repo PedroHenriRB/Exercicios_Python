6b0f0da978e93c5dd3ca93426932708aff360c17
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-25 17:29:06 -0300

    Update ex019.py

---
+++ basico/ex019.py
@@ -0,0 +1,10 @@
+import random
+
+aluno1 = input("Digite o 1° aluno: ")
+aluno2 = input("Digite o 2° aluno: ")
+aluno3 = input("Digite o 3° aluno: ")
+aluno4 = input("Digite o 4° aluno: ")
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+
+print(f"O aluno escolhido foi {random.choice(lista)}")
