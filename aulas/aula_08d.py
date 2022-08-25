44f6eff60153777e63ea9d9ff0d5eddd5dc60f11
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-25 14:03:29 -0300

    Update aula_08d.py

--- aulas/aula_08d.py
+++ aulas/aula_08d.py
@@ -1,4 +1,3 @@
-import emoji
+import emoji        #Para instalar uma biblioteca = pip install [nome do pacote](tudo no terminal) e pip list para ver a lista de pacotes instalados
 
 print(emoji.emojize('ola, mundo!:globe_showing_Americas:', language='alias'))
-
