0f7b12227ccf36104718a01f054d13db3e96f6fe
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-29 15:26:10 -0300

    Update ex021.py

--- basico/ex021.py
+++ basico/ex021.py
@@ -1,3 +1,15 @@
 from playsound import playsound
 
 playsound('D:\Downloads\Libera Me From Maromba.mp3')
+
+"""
+Ou você pode usar a biblioteca pygame:
+
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('D:\Downloads\Libera Me From Maromba.mp3)
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+"""
