14a08ed54fcf874c982034923647a170b86c200d
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-29 15:23:24 -0300

    Update ex021.py

--- basico/ex021.py
+++ basico/ex021.py
@@ -1,7 +1,3 @@
-import pygame
- 
-pygame.init()
-pygame.mixer.music.load('D:\Downloads\Libera Me From Maromba.mp3')
-pygame.mixer.music.play()
-input()
-pygame.event.wait()
+from playsound import playsound
+
+playsound('D:\Downloads\Libera Me From Maromba.mp3')
