aa37703e97a17e9ab943a2288295f10e8c28140f
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-30 15:25:06 -0300

    Update ex022.py

--- basico/ex022.py
+++ basico/ex022.py
@@ -1,6 +1,18 @@
-nome = str(input("Digite seu nome completo: "))
+nome = str(input("Digite seu nome completo: ")).strip()
 
 print(f"Maiúsculas:{nome.upper()}")
 print(f"Minúsculas:{nome.lower()}")
 print("Número de letras:", len(nome.replace(" ", "")))
 print("Número de letras do primeiro nome:", len(nome.split()[0]))
+
+# Ou
+
+"""
+nome = str(input("Digite seu nome completo: ")).strip()
+
+print(f"Maiúsculas:{nome.upper()}")
+print(f"Minúsculas:{nome.lower()}")
+print("Número de letras:", len(nome) - nome.count(" "))
+print("Número de letras do primeiro nome:", nome.find(" "))
+
+"""
