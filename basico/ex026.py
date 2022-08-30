c9db20ec8c016f12a2395841c86d64e36c339700
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-08-30 16:29:55 -0300

    Update ex026.py

--- basico/ex026.py
+++ basico/ex026.py
@@ -1,4 +1,4 @@
-frase = str((input("Digite uma frase: ")))
+frase = str((input("Digite uma frase: "))).strip().lower()
 
 print(f"A letra \"a\" aparece {frase.count('a')} vezes.")
 print(f"Posição primeira vez: {frase.find('a')}")
