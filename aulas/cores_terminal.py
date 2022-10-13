d0b08b27504077b44139e7c3cbcd8205f3f99d22
PedroHenriRB <111917136+PedroHenriRB@users.noreply.github.com>
2022-10-13 16:19:25 -0300

    Create cores_terminal.py

---
+++ aulas/cores_terminal.py
@@ -0,0 +1,14 @@
+##Cores no Terminal
+
+"""
+Text            Background          Style
+30      Black       40                0 - None
+31      Red         41                1 - Bold
+32      Green       42                4 - Underline
+33      Yellow      43                7 - Reverse Video
+34      Blue        44 
+35      Magenta     45
+36      Cyan        46
+37      Grey        47
+"""
+
